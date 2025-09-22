1253e38737073e9fd24452b0bf32b5736eaaf5d9
Swagtiago29 <sant29ramos@gmail.com>
2025-09-22 12:49:57 -0300

    add app

--- task_0/dashboard/src/App.jsx
+++ task_0/dashboard/src/App.jsx
@@ -1,11 +1,19 @@
 import './App.css'
-
+import Logo from './assets/holberton-logo.jpg'
 function App() {
 
-  return (
-    <>
-
-    </>
+  return (<>
+    <div className='App-header'>
+      <img src={Logo} alt='holberton-logo' />
+      <h1>School dashboard</h1>
+    </div>
+    <div className='App-body'>
+      <p>Login to access the full dashboard</p>
+    </div>
+    <div className='App-footer'>
+      <p>Copyright 2025 - holberton School</p>
+    </div>
+  </>
   )
 }
 
