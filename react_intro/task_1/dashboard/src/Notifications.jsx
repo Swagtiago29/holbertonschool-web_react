6578f6a115c47aeb177d686454a826406e72db03
Swagtiago29 <sant29ramos@gmail.com>
2025-09-22 16:27:05 -0300

    add task 1

---
+++ react_intro/task_1/dashboard/src/Notifications.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export function Notifications() {
+    return (
+        <div className='notification-items'>
+            <p>Here is the list of notifications</p>
+        </div>
+    )
+}
