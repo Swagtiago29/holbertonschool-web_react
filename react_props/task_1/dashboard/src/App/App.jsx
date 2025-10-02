3b0411947f256afb3ae7d122ab33847944815492
Swagtiago29 <sant29ramos@gmail.com>
2025-10-02 16:06:49 -0300

    add yask 1

---
+++ react_props/task_1/dashboard/src/App/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+import Notifications from '../Notifications/Notifications.jsx'
+import Login from '../Login/Login.jsx'
+import Footer from '../Footer/Footer.jsx'
+import Header from '../Header/Header.jsx'
+
+function App() {
+
+  return (
+    <>
+      <Notifications />
+      <Header />
+      <Login />
+      <Footer />
+    </>
+  )
+}
+
+export default App
---
+++ react_props/task_1/dashboard/src/App/App.spec.js
@@ -0,0 +1,25 @@
+import { render, screen } from '@testing-library/react'
+import '@testing-library/jest-dom'
+import App from './App'
+
+describe('App Component', () => {
+  test('renders Notifications component', () => {
+    render(<App />)
+    expect(screen.getByText(/notifications/i)).toBeInTheDocument()
+  })
+
+  test('renders Header component', () => {
+    render(<App />)
+    expect(screen.getByText(/school dashboard/i)).toBeInTheDocument()
+  })
+
+  test('renders Login component', () => {
+    render(<App />)
+    expect(screen.getByText(/login to access the full dashboard/i)).toBeInTheDocument()
+  })
+
+  test('renders Footer component', () => {
+    render(<App />)
+    expect(screen.getByText(/copyright/i)).toBeInTheDocument()
+  })
+})
