eeca2373986be3375f30555fc4efd3390e571c6b
Swagtiago29 <sant29ramos@gmail.com>
2025-10-03 14:11:34 -0300

    add task 3

---
+++ react_props/task_3/dashboard/src/Notifications/Notifications.jsx
@@ -0,0 +1,39 @@
+import React from 'react'
+import './Notifications.css'
+import CloseButton from '../assets/close-button.png'
+import { getLatestNotification } from '../utils/utils'
+import NotificationItem from './NotificationItem'
+
+function Notifications({ notifications = [] }) {
+    const ButtonStyle = {
+        position: 'absolute',
+        right: '10px',
+        top: '10px',
+        border: 'none',
+        background: 'none',
+        cursor: 'pointer',
+        padding: '0px 2px'
+    }
+    return (
+        <div className='root-notifications'>
+            <div className='notification-items'>
+                <button aria-label='Close'
+                    style={ButtonStyle}
+                    onClick={() => console.log('Close button has been clicked')}>
+                    <img src={CloseButton} width={'10px'} height={'10px'} />
+                </button>
+                <p>Here is the list of notifications</p>
+                <ul>
+                    {notifications.map((notif) => (
+                        <NotificationItem
+                            key ={notif.id}
+                            type={notif.type}
+                            value={notif.value}
+                            html={notif.html} />
+                    ))}
+                </ul>
+            </div>
+        </div>
+    )
+}
+export default Notifications
