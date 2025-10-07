64f9d3d30f4e4b44730ffec3a7ed4107f7a81b0a
Swagtiago29 <sant29ramos@gmail.com>
2025-10-07 14:15:06 -0300

    a

--- react_component/task_4/dashboard/src/HOC/WithLogging.jsx
+++ react_component/task_4/dashboard/src/HOC/WithLogging.jsx
@@ -1,7 +1,7 @@
 function WithLogging(WrappedComponent) {
     return class extends React.Component {
         componentDidMount() {
-
+            console.log(`Component ${} is mounted`)
         }
     }
 }
