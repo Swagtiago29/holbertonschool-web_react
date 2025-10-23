03bfd02f079c3e58965991e097353677c60d74dd
Swagtiago29 <sant29ramos@gmail.com>
2025-10-23 20:10:40 -0300

    fix .css

--- react_styling/task_3/dashboard/src/App/App.jsx
+++ react_styling/task_3/dashboard/src/App/App.jsx
@@ -1,5 +1,4 @@
 import React from 'react'
-import './App.css'
 import Notifications from '../Notifications/Notifications.jsx'
 import Login from '../Login/Login.jsx'
 import Footer from '../Footer/Footer.jsx'
