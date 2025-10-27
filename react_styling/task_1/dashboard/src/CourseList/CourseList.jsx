cbdfb82d0007ec2b9f013e7e16fa7502c2f1b278
Swagtiago29 <sant29ramos@gmail.com>
2025-10-27 16:55:21 -0300

    a

--- react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
+++ react_styling/task_1/dashboard/src/CourseList/CourseList.jsx
@@ -1,33 +1,46 @@
-import React from 'react'
-import CourseListRow from './CourseListRow'
+import React from 'react';
+import CourseListRow from './CourseListRow';
 
 function CourseList({ courses = [] }) {
-    return (
-        <div className='w-[85%] mx-auto my-8 overflow-x-auto'>
-            <table id='CourseList' className='w-full'>
-                {courses.length === 0 ? null :
-                    <thead>
-                        <CourseListRow isHeader={true} textFirstCell='Available courses' />
-                        <CourseListRow isHeader={true} textFirstCell='Course Name' textSecondCell='Credit' />
-                    </thead>}
+  return (
+    <div className="w-[85%] mx-auto my-8 overflow-x-auto">
+      <table id="CourseList" className="w-full border-collapse">
+        <thead>
+          {courses.length === 0 ? (
+            <CourseListRow
+              isHeader={true}
+              textFirstCell="No course available yet"
+            />
+          ) : (
+            <>
+              <CourseListRow
+                isHeader={true}
+                textFirstCell="Available courses"
+              />
+              <CourseListRow
+                isHeader={true}
+                textFirstCell="Course Name"
+                textSecondCell="Credit"
+              />
+            </>
+          )}
+        </thead>
 
-                <tbody>
-                    {courses.length === 0 ? (
-                        <CourseListRow isHeader={true} textFirstCell='No course available yet' />
-                    ) : (
-                        courses.map((course) => (
-                            <CourseListRow
-                                key={course.id}
-                                textFirstCell={course.name}
-                                textSecondCell={course.credit}
-                                isHeader={false}
-                            />
-                        ))
-                    )}
-                </tbody>
-            </table>
-        </div>
-    )
+        {courses.length > 0 && (
+          <tbody>
+            {courses.map((course) => (
+              <CourseListRow
+                key={course.id}
+                isHeader={false}
+                textFirstCell={course.name}
+                textSecondCell={course.credit}
+              />
+            ))}
+          </tbody>
+        )}
+      </table>
+    </div>
+  );
 }
 
-export default CourseList
+export default CourseList;
