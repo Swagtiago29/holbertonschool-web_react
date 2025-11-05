a826efb1c23e6d7836b3d0e8444795182b43dd5c
Swagtiago29 <sant29ramos@gmail.com>
2025-11-05 12:51:38 -0300

    fix

--- react_state/task_3/dashboard/src/Header/Header.jsx
+++ react_state/task_3/dashboard/src/Header/Header.jsx
@@ -11,28 +11,15 @@ class Header extends Component {
     return (
       <>
         <div className='App-header flex items-center flex-row max-[912px]:flex-col'>
-          <img
-            src={Logo}
-            alt='holberton logo'
-            className='w-60 max-[912px]:w-45'
-          />
+          <img src={Logo} alt='holberton logo' className='w-60 max-[912px]:w-45'/>
           <h1 className='font-bold text-[45px] text-(--main-color) max-[912px]:mb-8 max-[912px]:text-[37px]'>
             School Dashboard
           </h1>
         </div>
-
         {user && user.isLoggedIn && (
           <section id='logoutSection'>
             Welcome {user.email}{' '}
-            (<a
-              href='#'
-              onClick={(e) => {
-                e.preventDefault();
-                logOut();
-              }}
-            >
-              logout
-            </a>)
+            (<a href='#' onClick={(e) => {e.preventDefault(); logOut();}}>logout</a>)
           </section>
         )}
       </>
