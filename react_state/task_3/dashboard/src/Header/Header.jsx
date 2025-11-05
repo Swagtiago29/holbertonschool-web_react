f167aac4fada848a01d2c94d698f3e4908bdb382
Swagtiago29 <sant29ramos@gmail.com>
2025-11-05 12:46:56 -0300

    end 3

--- react_state/task_3/dashboard/src/Header/Header.jsx
+++ react_state/task_3/dashboard/src/Header/Header.jsx
@@ -1,18 +1,43 @@
-import React, { useContext } from 'react';
+import React, { Component } from 'react';
 import Logo from '../assets/holberton-logo.jpg';
-import Context from '../Context/context'
+import Context from '../Context/context';
 
-const Header = () => {
-    const { user, logOut } = useContext(Context)
+class Header extends Component {
+  static contextType = Context;
+
+  render() {
+    const { user, logOut } = this.context;
+
     return (
-        <>
-            <div className='App-header flex items-center flex-row max-[912px]:flex-col'>
-                <img src={Logo} alt='holberton logo' className='w-60 max-[912px]:w-45' />
-                <h1 className='font-bold text-[45px] text-(--main-color) max-[912px]:mb-8 max-[912px]:text-[37px]'>School Dashboard</h1>
-            </div>
-            {user && user.isLoggedIn && (<section id='logoutSection'>Welcome {user.email} (<a href='#' onClick={logOut}>Logout</a>)</section>)}
-        </>
-    )
+      <>
+        <div className='App-header flex items-center flex-row max-[912px]:flex-col'>
+          <img
+            src={Logo}
+            alt='holberton logo'
+            className='w-60 max-[912px]:w-45'
+          />
+          <h1 className='font-bold text-[45px] text-(--main-color) max-[912px]:mb-8 max-[912px]:text-[37px]'>
+            School Dashboard
+          </h1>
+        </div>
+
+        {user && user.isLoggedIn && (
+          <section id='logoutSection'>
+            Welcome {user.email}{' '}
+            (<a
+              href='#'
+              onClick={(e) => {
+                e.preventDefault();
+                logOut();
+              }}
+            >
+              logout
+            </a>)
+          </section>
+        )}
+      </>
+    );
+  }
 }
 
-export default Header
+export default Header;
