98305bedd05b24759664ba49df80f7b6d44815b4
Swagtiago29 <sant29ramos@gmail.com>
2025-11-08 13:15:39 -0300

    add task 5

---
+++ react_hooks/task_4/dashboard/src/Notifications/NotificationItem.spec.js
@@ -0,0 +1,16 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import NotificationItem from "./NotificationItem";
+import '@testing-library/jest-dom'
+
+describe('Tests for NotificationItem', () => {
+
+    test('should call markAsRead prop when click is triggered', () => {
+        const mockMarkAsRead = jest.fn()
+        render(<NotificationItem id={69} type="default" value="Click me" markNotificationAsRead={mockMarkAsRead} />)
+
+        const item = screen.getByText('Click me') 
+        fireEvent.click(item)
+
+        expect(mockMarkAsRead).toHaveBeenCalledTimes(1)
+    })
+})
